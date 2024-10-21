069d2107d63b3e4f0af5ed7f2f82878313d807ed
Nicole Fonseca <148062351+ehan1c@users.noreply.github.com>
2024-10-20 22:10:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Independência do Brasil?',
+    '1822'
+)
+
+criaCartao(
+    'Tecnologia',
+    'Qual é o nome do sistema operacional desenvolvido pela Microsoft?',
+    'Windows'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o elemento químico com o símbolo O?',
+    'Oxigênio'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de π (pi) arredondado para duas casas decimais?',
+    '3,14'
+)
+
+criaCartao(
+    'Música',
+    'Quem é o cantor da música "Billie Jean"?',
+    'Michael Jackson?'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Casmurro"?',
+    'Machado de Assis?'
+)
